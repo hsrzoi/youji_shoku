6f789819885d15a6a09ca9d3ac072e3d32fc8692
Miki Shibuya <mizy874@gmail.com>
2025-06-16 15:48:51 +0900

    ステップ5：ビューとURLの作成

--- meal/views.py
+++ meal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import MealPlan
 
-# Create your views here.
+def mealplan_list(request):
+    mealplans = MealPlan.objects.all().order_by('-date')
+    return render(request, 'meal/mealplan_list.html', {'mealplans': mealplans})
