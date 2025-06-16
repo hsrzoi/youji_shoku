b3da6461a286112cd3240d06ee537eedd76f190d
Miki Shibuya <mizy874@gmail.com>
2025-06-16 14:46:22 +0900

    ステップ2：mealアプリ作成とsettings.pyに登録

--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    is_allergen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    age_min_months = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.title
