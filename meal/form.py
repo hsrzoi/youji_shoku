7ea8251d53ef4f1d80fff78453c9c619fa1a080b
Miki Shibuya <mizy874@gmail.com>
2025-06-16 15:50:22 +0900

    ステップ6：IngredientForm と MealPlanForm を作成

---
+++ meal/form.py
@@ -0,0 +1,13 @@
+# meal/forms.py
+from django import forms
+from .models import Ingredient, MealPlan
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'category', 'expiration_date']
+
+class MealPlanForm(forms.ModelForm):
+    class Meta:
+        model = MealPlan
+        fields = ['date', 'meal_type', 'menu']
